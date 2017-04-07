dd9b0692d49eb5f8bd7f387aa384ceca38f18241
Valeria Benetti <vbenetti46@gmail.com>
2017-04-06 22:50:50 -0400

    Time stamp and link created

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,7 @@
 class StoriesController < ApplicationController
   # GET /stories
   def index
-    @stories = Story.all
+    @stories = Story.all.sort_by { |obj| obj.created_at }
   end
 
   # GET /stories/1
