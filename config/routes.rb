79e6a0e1fcf9bd4352ce1f1f036c6723a74cdc53
Valeria Benetti <vbenetti46@gmail.com>
2017-04-11 18:54:07 -0400

    Added user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 end
